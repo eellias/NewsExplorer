65369e8e86cbdaa77fe89bd41f46fd9917e303cf
Illia Tovstokoryi <ilya.tovstokory@gmail.com>
2023-06-11 20:40:43 +0300

    Initial Commit

---
+++ NewsExplorer/NewsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsExplorerApp.swift
+//  NewsExplorer
+//
+//  Created by Ilya Tovstokory on 11.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
